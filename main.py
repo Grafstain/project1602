1308932ea42faf5982e577647753a3a701fc7f2e
Grafstain <grafstain@gmail.com>
2023-02-16 21:05:28 +0300

    count true and false for throwing a coin 100 times

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-#  child with question why?
+#  count true and false for throwing a coin 100 times
 # ©Grafstain
 
-answer = ""
-print("Привет сынок")
-print("Мам у меня есть вопрос, почему небо синее?")
-while True:
-    answer = input()
-    if answer.lower() == "потому что":
-        print("А, понятно")
-        break
-    else:
-        print("Почему " + answer.lower() + "?")
+import random
 
+i, count_true, count_false = 0, 0, 0
+while i < 100:
+
+    throw = random.randint(0, 1)
+    if throw:
+        count_true += 1
+        print("Выпал Орел")
+    else:
+        count_false += 1
+        print("Выпала Решка")
+    i += 1
+print("Всего выпало орлов: ", count_true, "\nВсего выпало решек: ", count_false)
